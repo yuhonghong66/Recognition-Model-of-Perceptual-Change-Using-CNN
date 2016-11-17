33ff9cd20d962865d8b285087e7f6d00ca062bb0
K0k1Nakamura <k00ook17@gmail.com>
2016-11-17 15:46:52 +0900

    Enable visualize attention image

--- models/critical_dynamics_model.py
+++ models/critical_dynamics_model.py
@@ -15,9 +15,9 @@ class CriticalDynamicsModel(chainer.Chain):
     """Input dimensions are (128, 128)."""
     def __init__(self, nobias=True):
         super(CriticalDynamicsModel, self).__init__(
-            conv1=L.Convolution2D(3, 64, 3, stride=2, nobias=nobias),
-            conv2=L.Convolution2D(64, 64, 3, stride=2, nobias=nobias),
-            conv3=L.Convolution2D(64, 128, 4, stride=1, nobias=nobias),
+            conv1=L.Convolution2D(3, 64, 4, stride=2, nobias=nobias, pad=1),
+            conv2=L.Convolution2D(64, 64, 4, stride=2, nobias=nobias, pad=1),
+            conv3=L.Convolution2D(64, 128, 3, stride=1, nobias=nobias),
             attention=L.Linear(2, 1152),
             # fc6=L.Linear(25088, 4096),
             fc6=L.Linear(1152, 2),
@@ -96,6 +96,31 @@ class CriticalDynamicsModel(chainer.Chain):
         else:
             self.pred = F.softmax(h)
             return self.pred
+
+    def activate_with_attention(self, x, a):
+        if x.data.shape[0] != 1:
+            raise TypeError('Visualization is only supported for a single \
+                            image at a time')
+        self.add_deconv_layers()
+        # Forward pass
+        h = self.forward_with_attention(x, a, stop_layer='attention')
+        xp = chainer.cuda.get_array_module(h.data)
+        layer = len(self.convs)
+        deconvs = [['de{}'.format(c) for c in conv] for conv in self.convs]
+
+        feat_maps = []
+
+        for i, deconv in enumerate(reversed(deconvs)):
+            h = F.unpooling_2d(h, self.switches[layer-i-1], 2, stride=2,
+                               outsize=self.unpooling_outsizes[layer-i-1])
+            for d in reversed(deconv):
+                h = getattr(self, d)(F.relu(h))
+
+        feat_maps.append(h.data)
+        feat_maps = xp.array(feat_maps)
+        feat_maps = xp.rollaxis(feat_maps, 0, 2)  # Batch to first axis
+
+        return Variable(feat_maps)
 
     def activations(self, x, layer):
         if x.data.shape[0] != 1:
